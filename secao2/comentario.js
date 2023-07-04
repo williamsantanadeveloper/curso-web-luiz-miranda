f45c186b16f52252ff7be45abd0a50daa1b80ec0
William Santana <williamandrade1058@gmail.com>
2023-07-04 15:19:40 -0300

    update comentário

---
+++ secao2/comentario.js
@@ -0,0 +1,11 @@
+// E escrever um comentário
+console.log("Olá mundo!"); // Aqui tem outro comentário
+console.log("linha 1");
+console.log("linha 2");
+// Comentário
+console.log("linha 3");
+console.log("linha 4");
+// Vai ser ignorado
+/* 
+Um bloco de código
+*/
