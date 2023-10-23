77fe4a7b06de4f208e1d8f5801717cb35183a59d
William Santana <williamandrade1058@gmail.com>
2023-10-23 14:53:00 -0300

    add usando_let.js

---
+++ js/aula6/anotacões.js
@@ -0,0 +1,14 @@
+// Não podemos criar variáveis com palavras reservadas
+// ex: let if;, let console;
+
+// Variáveis precisam ter nomes significativos
+let client_name = 'João';
+console.log(client_name);
+
+// Não pode começar o nome de uma variável com um número
+let 1name;
+// Não podem conter espaços ou traços
+let client name;
+
+// Utilizamos camelCase
+let fullName = 'Luiz Otávio';
---
+++ js/aula6/usando_let.js
@@ -0,0 +1,17 @@
+let nome; // Declarou a variável
+nome = 'João'; // Inicializando a variável
+console.log(nome);
+
+nome = 'Otávio'
+console.log(nome)
+
+// let nome; // erro
+console.log('teste')
+// let nome = 'Henrique'; // String
+
+// console.log(nome, ' nasceu em 1984');
+// console.log('Em 2000 ', nome, ' conheceu Maria.');
+// console.log(nome, ' casou-se com maria em 2012');
+// console.log('Maria teve um filho com ', nome, ' em 2015');
+// console.log('O filho de', nome, 'se chama Eduardo');
+
