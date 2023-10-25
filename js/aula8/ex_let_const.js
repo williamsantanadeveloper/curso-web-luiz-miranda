aa147a6004231e047c5924831a019ace4978814b
William Santana <williamandrade1058@gmail.com>
2023-10-25 20:51:15 -0300

    add ex_let_const.js

--- js/aula8/ex_let_const.js
+++ js/aula8/ex_let_const.js
@@ -1,5 +1,5 @@
 const name = 'William Batista';
-const surname = ' Andrade';
+const surname = 'Andrade';
 let age = 18;
 let weight = 95;
 let height = 1.73;
@@ -9,4 +9,6 @@ let year_of_birth = 2023 - age;
 // + 
 // template strings
 
-console.log(`${name}`, surname.concat('tem'), age + ' anos, pesa ' + weight + ' kg' + ' tem ' + height + ' de altura e seu IMC é de ' + Math.round(imc, 2) + ` ele nasceu no ano em ` + year_of_birth);
+console.log(`${name}, ${surname} tem ${age} anos, pesa ${weight} kg `);
+console.log(`tem ${height} de altura e seu IMC é de ${Math.round(imc)}`);
+console.log(`${name} nasceu em ${year_of_birth}.`);
