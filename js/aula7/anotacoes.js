e7f6e1b0e1d06781fd35b3135ec4c683df6b7593
William Santana <williamandrade1058@gmail.com>
2023-10-25 11:19:34 -0300

    add usando_const.js

---
+++ js/aula7/anotacoes.js
@@ -0,0 +1,21 @@
+// Não podemos criar variáveis com palavras reservadas
+// ex: const if;, const console;
+
+// Constantes precisam ter nomes significativos
+const client_name = 'João';
+console.log(client_name);
+
+// Não pode começar o nome de uma constante com um número
+const 1name;
+// Não podem conter espaços ou traços
+const client name;
+
+// Utilizamos camelCase
+const fullName = 'Luiz Otávio';
+
+// Case-sensitive
+const clientName = 'Luiz';
+const clientname = 'Otávio';
+
+// Não podemos modifica o valor de uma constante
+// Não utilize var, utilize const
---
+++ js/aula7/usando_const.js
@@ -0,0 +1,19 @@
+// const name;
+const name = 'João';
+console.log(name);
+
+// name = 'Otávio';
+// console.log(name)
+
+const firstNumber = 5;
+const secondNumber = 10;
+const result = firstNumber * secondNumber;
+const doubleResult = 2 * result;
+let tripleResult = 3 * result;
+tripleResult += 5;
+console.log(result);
+console.log(doubleResult);
+console.log(tripleResult);
+
+console.log(typeof (firstNumber));
+// + - * /
