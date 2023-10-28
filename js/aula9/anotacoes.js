61dd64bd2dba7052382989e6f6098ec857088f07
William Santana <williamandrade1058@gmail.com>
2023-10-27 21:05:45 -0300

    add var_vs_let.js

---
+++ js/aula9/anotacoes.js
@@ -0,0 +1,4 @@
+// Com var não contêm erro em redeclaração de variável, mas no let isso não é possível
+var name = 'William';
+var name = 'Batista'
+console.log(`${name}`);
---
+++ js/aula9/var_vs_let.js
@@ -0,0 +1,12 @@
+// ECMAScript 2015 (ES6) - padronização da linguagem JavaScript
+// var name = 'William';
+// var name = 'Batista';
+
+// console.log(`${name}`);
+
+// nome = 'luiz'; // não faça isso
+// console.log(nome)
+
+let name = 'William';
+let name = 'William';
+console.log(name);
