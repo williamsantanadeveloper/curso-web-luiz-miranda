32b3f58f5847fa81ce26f3d382f18986d5da048b
William Santana <williamandrade1058@gmail.com>
2023-11-01 11:42:35 -0300

    add operadores.js

---
+++ js/aula11/anotacoes.js
@@ -0,0 +1,30 @@
+/* 
+Aritméticos + - / * ** %
++ - Adição / Concatenação
+- - subtração
+/ - Divisão
+* - multiplicação
+** - potenciação
+% - resto da divisão
+
+Ordem de precedência
+()
+**
+* / %
++ -
+
+Incremento = ++
+Decremento = --
+
+Operadores de atribuição
++=
+-=
+*=
+**=
+
+NaN - Not a number
+
+const number2 = parseInt('5'); (conversão para inteiro)
+const number2 = parseFloat('5.2'); (conversão para decimal)
+const number2 = Number('5.2'); (conversão para um número)
+*/
---
+++ js/aula11/index.js
@@ -0,0 +1,42 @@
+const num1 = 5;
+const num2 = 10;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 ** num2);
+console.log(num1 % num2);
+
+const num3 = 2;
+console.log((num1 + num2) * num3);
+
+const passo = 2;
+let contador = 0;
+contador++;
+contador++;
+console.log(contador);
+
+// let contador = 1;
+// console.log(contador++);
+// console.log(contador);
+
+// let contador = 1;
+// console.log(++contador);
+
+// let contador = 10;
+// console.log(--contador);
+
+contador += passo;
+console.log(contador);
+
+contador = 2;
+contador **= 10;
+console.log(contador);
+
+const number1 = 10;
+// const number2 = parseInt('5');
+const number2 = Number('5.2');
+
+console.log(number1 + number2);
+console.log(typeof number2);
