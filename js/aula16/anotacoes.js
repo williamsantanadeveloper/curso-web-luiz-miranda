a5dfe570b79931138eaa12887b1bd1ddd8d2cd23
William Santana <williamandrade1058@gmail.com>
2023-11-19 10:24:00 -0300

    add aula 16

---
+++ js/aula16/anotacoes.js
@@ -0,0 +1,19 @@
+/* 
+Objeto Math
+
+let num1 = 9.54578;
+let num2 = Math.floor(num1); // Arredondando para baixo
+let num3 = Math.ceil(num1); // Arredondando para cima
+let num3 = Math.round(num1); // Arredondando para o número mais próximo ex: 9.49 - 9 / 9.51 - 10
+
+console.log(Math.max(1, 2332, 32, 3, 2, 32, 3, 32, 32, 32, 5, -23.32, -100)); - Mostrando o maior número
+console.log(Math.min(1, 2332, 32, 3, 2, 32, 3, 32, 32, 32, 5, -23.32, -100)); - Mostrando o menor número
+console.log(Math.random()); - Gera um número aleatório entre 0 até 1 só que o 1 não é incluído
+const random = Math.random() * (10 - 5) + 5; - Gerando um número aleatório entre 5 e 10
+console.log(Math.PI); - valor de PI : 3.14
+
+console.log(Math.pow(2, 3)); - potenciação
+console.log(Math.sqrt(4)); - raiz quadrada
+
+console.log(100 / 0); - ifinito cuidado!!!
+*/
---
+++ js/aula16/index.js
@@ -0,0 +1,12 @@
+// let num1 = 9.24578;
+// let num2 = Math.floor(num1); // Arredondando para baixo
+// // let num3 = Math.ceil(num1); // Arredondando para cima
+// let num3 = Math.round(num1); // Arredondando para o número mais próximo ex: 9.49 - 9 / 9.51 - 10
+// console.log(num2, num3);
+
+// const random = Math.round(Math.random() * (10 - 5) + 5);
+// console.log(random);
+
+// console.log(Math.pow(2, 3));
+
+console.log(100 / 0);
